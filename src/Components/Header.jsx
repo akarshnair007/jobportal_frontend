cd2a51ffe1e99d440b1410bafc087e4a03d2efe7
Akarsh Nair <akarshnair007@gmail.com>
2024-06-02 17:07:57 +0530

    Added some responsiveness to header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="flex flex-wrap justify-between items-center w-full py-4 px-4 sm:px-8 bg-[#07001f]">
       <motion.img
-        className="h-7 sm:h-8"
+        className="h-6 sm:h-8"
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 100 }}
         transition={{ delay: 0.3, duration: 0.8 }}
@@ -16,7 +16,7 @@ const Header = () => {
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 100 }}
         transition={{ delay: 0.3, duration: 0.8 }}
-        className="flex gap-5 mt-4 sm:mt-0"
+        className="flex gap-5  sm:mt-0"
       >
         <button className="bg-[#204299] text-white text-sm sm:text-lg px-2 py-1 rounded-md w-full sm:w-auto">
           Admin Login
