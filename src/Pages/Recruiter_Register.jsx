d539a3d40fcfde5d1a629d87740355fab2e94375
Akarsh Nair <akarshnair007@gmail.com>
2024-07-15 03:09:34 +0530

    Updated Recruiters Page

--- src/Pages/Recruiter_Register.jsx
+++ src/Pages/Recruiter_Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../Components/Header";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { recruiterRegisterAPI } from "../Services/AllApi";
@@ -12,6 +12,8 @@ const Recruiter_Register = () => {
     password: "",
     organization_name: "",
   });
+
+  const navigate = useNavigate();
 
   const submitHandler = async (e) => {
     e.preventDefault();
